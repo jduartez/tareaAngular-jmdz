c44d85260d437d56fe8f642a76c7cafa12b76404
JDuarte <jmduartezavala@gmail.com>
2024-08-22 23:33:36 -0400

    Entrega Final Test componente login JMDZ up

--- src/app/shared/directives/fontsize.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FontsizeDirective } from './fontsize.directive';
-
-describe('FontsizeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FontsizeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
